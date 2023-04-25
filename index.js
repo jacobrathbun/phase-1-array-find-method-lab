b57c04c5526ab9f2361b532be0ad711474bd7152
Jacob Rathbun <jrathofbun@gmail.com>
2023-04-25 18:41:01 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(years) {
+    const win = years.find(win => win.result === "W");
+    if(win) {
+        return win.year;
+    }
+    else {
+        return undefined;
+    }
+}
